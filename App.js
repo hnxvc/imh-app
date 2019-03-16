df24a7d08c642a71373776f82c5ec92e6b2bfda1
nguyenxuanhoa94 <hoanguyen.fullstack@gmail.com>
2019-03-16 15:35:35 +0700

    Add icon

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 
 import { HomeScreen, CategoriesScreen } from './app/screens/Index';
 
 const bottomTabNavigator = createBottomTabNavigator({
-  Home: HomeScreen,
-  Categories: CategoriesScreen
+    Home: HomeScreen,
+    Categories: CategoriesScreen,
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let IconComponent = Ionicons;
+        let iconName;
+        // FIXME: change it work on ios, adroind
+        if (routeName === 'Home') {
+          iconName = 'ios-home';
+        } else if (routeName === 'Settings') {
+          iconName = 'ios-category';
+        }
+
+        return <IconComponent name={iconName} size={25} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
+  }
 });
 
 export default  createAppContainer(bottomTabNavigator);
